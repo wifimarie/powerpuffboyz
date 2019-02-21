1007f8fabf399c10ba566c941bd197fc67ff4ca8
mario <46033731+wifimarie@users.noreply.github.com>
2019-02-21 12:16:37 -0400

    Como funcionan los eventos

---
+++ eventos/eventos/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eventos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_SizeChanged(object sender, EventArgs e)
+        {
+MessageBox.Show("Esto es un ziz changed");
+        }
+
+        private void textBox1_FontChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("La font ha cambiado");
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void textBox1_BackColorChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Su color ha cambiado");
+        } 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.Pink;
+        }
+    }
+}
---
+++ proyecto_marie/proyecto_marie/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_marie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var Edad = 25;
+            //MessageBox.Show("Su edad es " + Edad);
+
+            // constante
+            const int impuesto = 18;
+            MessageBox.Show("El impuesto es es " + impuesto);
+            }
+        var = 
+        }
+    }
+
